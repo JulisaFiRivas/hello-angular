8b8d9fd196d8898222654e1ded73e82ff17d18d3
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-01-08 12:21:35 -0300

    Cambios a nueva version

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import {HousingService} from '../housing.service';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: ` 
+  <section>
+  <form>
+    <input type="text" placeholder="Filter by city">
+    <button class="primary" type="button">Search</button>
+  </form>
+</section>
+<section class = "results">
+  <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+</section>
+`,
+  styleUrls: ['./home.component.css'],
+})
+
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+
+  // Usando el nuevo API de inyección directamente
+  housingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
+/*export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingLocation = inject(HousingService);
+  
+  }*/
+
